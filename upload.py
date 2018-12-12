9333313bba2266cccbc2ba38a8b6491d5f8def38
zhaofeng-shu33 <616545598@qq.com>
2018-12-12 23:32:32 +0800

    open in binary mode

--- upload.py
+++ upload.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     auth = oss2.Auth(access_key_id, access_key_secret)
     bucket = oss2.Bucket(auth, 'http://oss-cn-shenzhen.aliyuncs.com', 'programmierung')
     # open `main.pdf` and `clustering.pdf` and upload them to the oss server
-    main_pdf = open('main.pdf')
-    clustering_pdf = open('clustering.pdf')
+    main_pdf = open('main.pdf','rb')
+    clustering_pdf = open('clustering.pdf','rb')
     research_base = 'research/info-clustering/'
     bucket.put_object(research_base + 'main.pdf', main_pdf)
     bucket.put_object(research_base + 'clustering.pdf', clustering_pdf)    
