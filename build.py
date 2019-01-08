950a19c2937dc4f1854e1814816774fd709915b4
zhaofeng-shu33 <616545598@qq.com>
2019-01-08 14:49:03 +0800

    distinguish publish and draft mode

---
+++ build.py
@@ -0,0 +1,28 @@
+# build individual files
+# 1. copy the source files to build dir
+# 2. cd to build dir
+# 3. write necessary headers
+# 4. build the pdf files
+import argparse
+import os
+from shutil import copyfile
+BUILD_DIR = 'build'
+MACRO = 'macros.tex'
+HEADER = '\documentclass{article}\input{%s}\\begin{document}\input{%s}\end{document}'
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='input file')
+    args = parser.parse_args()
+    print('step 1: copying files')
+    copyfile(args.file, os.path.join(BUILD_DIR, args.file))
+    copyfile(MACRO, os.path.join(BUILD_DIR, MACRO))
+    print('step 2: changing directory to ' + BUILD_DIR)
+    os.chdir(BUILD_DIR)
+    print('step 3: write necessary headers')
+    output_file_name = args.file.replace('.','_r.')
+    with open(output_file_name, 'w') as f:
+        f.write(HEADER % (MACRO, args.file))
+    print('step 4: build pdf files')
+    os.system('xelatex -synctex=1 %s' % output_file_name)
+        
+    
