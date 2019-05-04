feb33c0ce065df00b208d65ec78fc1074c7aa2a4
zhaofeng-shu33 <616545598@qq.com>
2019-05-04 21:32:04 +0800

    get a prefered two-level graph

--- sample/experiment_two_level.py
+++ sample/experiment_two_level.py
@@ -1,5 +1,19 @@
-# two level hierachical clustering, info-clustering algorithm can recover the original graph with 
-# partition_num_list = [1, k2, k2*k1, n*k2*k1]
+'''
+In this experiment, we prepare a graph with two level hierachical structures.
+The graph has k2 macro-communities and in each macro-community there are k1 micro-communities. Each micro-community
+contains n nodes. There are also two parameters p1 and p2 which control the inter-community linkage respectively.
+Ideally, when p2=0, the graph has k2 connected components and when p2=1, the graph is random at macro-community level.
+p1 controls the micro-community linkage in the same manner.
+Notice that p1 cannot be zero, otherwise the two-level structure of the graph does not exist.
+By using info-clustering algorithm we try to recover the original graph with 
+partition_num_list = [1, k2, k2*k1, n*k2*k1] under certain conditions of p1 and p2.
+
+We also use other clustering algorithm which does not require predetermined cluster number. 
+For example, Girvan-Newman algorithm and Affinity Propogation. There algorithms can get the macro-community first
+and we rerun the algorithm to get the micro-community structures.
+
+As we can see, if the graph has deep hierachical structure, info-cluster has advantages since we only need to run ic algorithm once.
+'''
 from info_cluster import InfoCluster
 import random
 import numpy as np
@@ -7,80 +21,86 @@ import argparse
 import networkx as nx # for manipulating graph data-structure
 import graphviz # for writing .gv file
 import pdb
-n = 12
-k1 = 7
-k2 = 3
 
-def construct(p1):
+n = 16
+k1 = 4
+k2 = 4
+K = 18
+color_list = ['red','orange','green','purple']
+def construct(z_in_1, z_in_2, z_out):
     '''
-       p1: type float, percentage of edges to be removed at first level.
+       p2: type float, percentage of edges to be added at macro level.
+       p1: type float, percentage of edges to be added at micro level.
     '''
     global n,k1,k2
+    p_1 = z_in_1/(n-1)
+    
+    assert(p_1 <= 1)
+    assert(z_out > 0)
+    
+    p_2 = z_in_2/(n*(k1-1))
+    p_o = z_out/(n*k1*(k2-1))
     G = nx.Graph()
-    # first level
-    for t in range(k1*k2):
-        for i in range(n):
-            for j in range(i+1,n):
-                G.add_edge(t*n+i,t*n+j)    
-    # second level
+    cnt = 0
     for t in range(k2):
         for i in range(k1):
-            for j in range(i+1,k1):
-                G.add_edge(t*n*k1+i*n, t*n*k1+j*n) # link the first vertice together
-
-    cnt = 0
-    a = 0
-    b = 0
-    choice_array = [i for i in range(k1*k2)]
-    while(cnt < k1*(k2-1)/2 - 1):
-        a1,b1 = np.random.choice(choice_array,2)
-        if(a1 == b1):
-            continue
-        if(a1 < b1):
-            a = a1
-            b = b1
-        else:
-            a = b1
-            b = a1
-        if(G.has_edge(a*n, b*n)):
-            continue  
-        a = a1
-        b = b1
-        cnt += 1
-        G.add_edge(a*n,b*n)
-        print('add edge', a*n, b*n)
+            for j in range(n):
+                G.add_node(cnt, macro=t,micro=i)
+                cnt += 1
+    for i in G.nodes(data=True):
+        for j in G.nodes(data=True):
+            if(j[0] <= i[0]):
+                continue
+            if(i[1]['macro'] != j[1]['macro']):
+                if(random.random()<=p_o):
+                    G.add_edge(i[0], j[0])
+            else:
+                if(i[1]['micro'] == j[1]['micro']):
+                    if(random.random() <= p_1):
+                        G.add_edge(i[0], j[0])
+                else:
+                    if(random.random() <= p_2):
+                        G.add_edge(i[0], j[0])
     return G    
 def graph_plot(G):
-    # G: networkx graph object
+    '''
+    generate the plot file which is the input of graphviz.
+    G: networkx graph object
+    '''
     global n, k1,k2
     g = graphviz.Graph(filename='two_level.gv', engine='neato') # g is used for plotting
-    for i in range(n*k1*k2):
-        g.node(str(i), shape='point')
+    for i in G.nodes(data=True):
+        macro_index = i[1]['macro']
+        g.node(str(i[0]), shape='point', color=color_list[macro_index])
     for e in nx.edges(G):
         i,j = e
-        if(abs(i-j) < n):
+        i_attr = G.node[i]
+        j_attr = G.node[j]
+        if(i_attr['macro']!=j_attr['macro']):
+            edge_len = 2
+            weight_value = 0.1
+        elif(i_attr['micro']!=j_attr['micro']):
             weight_value = 1
-        elif(abs(i-j) < n*k1):
-            weight_value = 0.2
+            edge_len = 1
         else:
-            weight_value = 0.04
-        g.edge(str(i), str(j), weight=str(weight_value))    
+            weight_value = 10
+            edge_len = 0.5
+        g.edge(str(i), str(j), weight=str(weight_value), penwidth="0.3", len=str(edge_len))    
     g.save()    
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--save_graph', default=False, type=bool, nargs='?', const=True, help='whether to save the .gv file') 
-    parser.add_argument('--print_tree', default=False, type=bool, nargs='?', const=True, help='whether to print the hierachical tree')
-    parser.add_argument('--p1', default=0.0, type=float, help='first level random parameter')      
+    parser.add_argument('--ic', default=False, type=bool, nargs='?', const=True, help='whether to run info-cluster algorithm')
+    parser.add_argument('--z_in_1', default=14, type=int, help='inter-micro-community node average degree')      
+    parser.add_argument('--z_in_2', default=3, type=int, help='intra-micro-community node average degree')          
     args = parser.parse_args()
     
-    ic = InfoCluster(affinity='precomputed')
-    G = construct(args.p1)    
+    G = construct(args.z_in_1, args.z_in_2, K - args.z_in_1 - args.z_in_2)    
     if(args.save_graph):
         graph_plot(G)
-    # use neato to plot the tree
-    sparse_mat = nx.adjacency_matrix(G)
-    ic.fit(np.asarray(sparse_mat.todense(),dtype=float))
-    print(ic.partition_num_list)
-    if(args.print_tree):        
-       ic.print_hierachical_tree()        
+    if(args.ic):           
+        ic = InfoCluster(affinity='precomputed')        
+        sparse_mat = nx.adjacency_matrix(G)
+        ic.fit(np.asarray(sparse_mat.todense(),dtype=float))
+        print(ic.partition_num_list)
         
