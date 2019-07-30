aa7297e9a44933f0a120a7323ebf17c90204d97d
zhaofeng-shu33 <616545598@qq.com>
2019-07-30 19:18:11 +0800

    add save function

--- experiment/community_detection/reproduce_experiment.py
+++ experiment/community_detection/reproduce_experiment.py
@@ -6,18 +6,48 @@ import bhcd_parameter
 
 import runner
 runner.NUM_TIMES = 20
+METRIC = 'dendrogram_purity'
 
-def reproduce_z_in_1(alg_list, metric):
-    for alg in alg_list:
-        runner.collect_z_in_1_evaluate(alg, 3, 1, 10, 15, metric)
-        
-def reproduce_z_in_2(alg_list, metric):
-    for alg in alg_list:
-        runner.collect_z_in_2_evaluate(alg, 13, 0.5, 2, 5, metric)
-        
-def reproduce_z_o(alg_list, metric):
-    for alg in alg_list:
-        runner.collect_z_o_evaluate(alg, 14, 3, 0.25, 2)
+def initialize_alg(alg_name):
+    if(alg_name == 'info-clustering'):
+        return InfoClusterWrapper()
+    elif(alg_name == 'gn'):
+        return GN()
+    elif(alg_name == 'bhcd'):
+        return BHCDWrapper()
+    else:
+        raise NotImplementedError(alg_name + ' not found')
+
+def reproduce_z_in_1(alg_name_list, metric):
+    Z_IN_2 = 3
+    Z_O = 1
+    Z_IN_1_MIN = 10
+    Z_IN_1_MAX = 15
+
+    for alg_name in alg_name_list:
+        alg = initialize_alg(alg_name)
+        report_list = runner.collect_z_in_1_evaluate(alg, Z_IN_2, Z_O, Z_IN_1_MIN, Z_IN_1_MAX, metric)
+        runner.save_to_file(report_list, 'z_in_1', runner.NUM_TIMES, alg_name, Z_IN_2, Z_O, Z_IN_1_MIN, Z_IN_1_MAX)
+
+def reproduce_z_in_2(alg_name_list, metric):
+    Z_IN_1 = 13
+    Z_O = 0.5
+    Z_IN_2_MIN = 2
+    Z_IN_2_MAX = 5
+    for alg_name in alg_name_list:
+        alg = initialize_alg(alg_name)
+        report_list = runner.collect_z_in_2_evaluate(alg, Z_IN_1, Z_O, Z_IN_2_MIN, Z_IN_2_MAX, metric)
+        runner.save_to_file(report_list, 'z_in_2', runner.NUM_TIMES, alg_name, Z_IN_1, Z_O, Z_IN_2_MIN, Z_IN_2_MAX)
+
+def reproduce_z_o(alg_name_list, metric):
+    Z_IN_1 = 14
+    Z_IN_2 = 3
+    Z_O_MIN = 0.25
+    Z_O_MAX = 2
+    for alg_name in alg_name_list:
+        alg = initialize_alg(alg_name)
+        report_list = runner.collect_z_o_evaluate(alg, Z_IN_1, Z_IN_2, Z_O_MIN, Z_O_MAX, metric)
+        runner.save_to_file(report_list, 'z_o', runner.NUM_TIMES, alg_name, Z_IN_1, Z_IN_2, Z_O_MIN, Z_O_MAX)
 
 class BHCDWrapper(BHCD):
     def __init__(self):
@@ -25,8 +55,7 @@ class BHCDWrapper(BHCD):
             gamma=bhcd_parameter.gamma, _lambda=bhcd_parameter._lambda, delta=bhcd_parameter.delta)
             
 if __name__ == '__main__':
-    alg_list = [InfoClusterWrapper(), GN(), BHCDWrapper()]
-    metric = 'norm_rf'
-    reproduce_z_in_1(alg_list, metric)
-    reproduce_z_in_2(alg_list, metric)
-    reproduce_z_o(alg_list, metric)    
+    alg_list = ['info-clustering']
+    reproduce_z_in_1(alg_list, METRIC)
+    reproduce_z_in_2(alg_list, METRIC)
+    reproduce_z_o(alg_list, METRIC)    
